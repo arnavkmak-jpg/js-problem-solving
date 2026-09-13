55d0629a2add9cc8f624b2ad11a5102eaa795992
arnavkmak-jpg <arnavkmak@gmail.com>
2026-09-13 21:42:40 +0530

    Dice Roller program using JavaScript

---
+++ dice roller/index.js
@@ -0,0 +1,16 @@
+function rollDice(){
+    const diceNumber = document.getElementById("diceNumber").value;
+    const diceResult = document.getElementById("diceResult");
+    const diceImages = document.getElementById("diceImages");
+    const values = [];
+    const images = [];
+
+    for(let i=0; i<diceNumber; i++){
+        const value = Math.floor(Math.random()*6)+1;
+        values.push(value);
+        images.push(`<img src="images/${value}.png" alt="Dice ${value}">`)
+    }
+
+    diceResult.textContent = `dice: ${values.join(`, `)}`;
+    diceImages.innerHTML = images.join(``)
+}
